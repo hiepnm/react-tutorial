dd97c649e48585948408ba9a6ede974ac09fe4a8
hiepnm <hiepnm2909@gmail.com>
2016-11-10 19:35:56 +0700

    Solve all problems

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ const Switch = (props) => (
 );
 //Square is stateless functional component
 const Square = (props) => (
-  <button className="square" onClick={() => props.onClick()}>
+  <button className="square" style={{backgroundColor: props.win ? "yellow" : "white"}} onClick={() => props.onClick()}>
     {props.value}
   </button>
 )
 
 class Board extends Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square value={this.props.squares[i]} win={this.props.winner.indexOf(i) !== -1} onClick={() => this.props.onClick(i)} />;
   }
   
   render() {
@@ -59,7 +59,6 @@ class App extends React.Component {
     };
   }
   render() {
-    console.log("ascending: " + this.state.ascending);
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
@@ -82,6 +81,7 @@ class App extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            winner={winner || []}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
@@ -147,7 +147,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i].slice();
     }
   }
   return null;
